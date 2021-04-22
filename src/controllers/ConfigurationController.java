148144cc1be9805a067ab84fe124e74b1b5dd3aa
Alex Wang <j3ang@gmail.com>
2021-04-21 21:20:07 -0500

    added parameter sliders

--- src/controllers/ConfigurationController.java
+++ src/controllers/ConfigurationController.java
@@ -1,21 +1,28 @@
 package controllers;
 
 import application.Main;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import models.ConfigModel;
 import models.UserModel;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
 
 public class ConfigurationController implements Initializable  {
     @FXML
@@ -24,10 +31,87 @@ public class ConfigurationController implements Initializable  {
     private VBox loggedinUserVBox;
     @FXML
     private VBox vTop;
+    @FXML
+    private VBox configs;
+    @FXML
+    private AnchorPane configsAnchor;
+
+
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initData();
+        setUpParams(new ConfigModel());
+
+    }
+
+    private void setUpParams(ConfigModel config) {
+        for ( int i = 0; i <  configsAnchor.getChildren().size(); i++ ){
+            String id = configsAnchor.getChildren().get(i).getId();
+            try {
+                switch (id) {
+                    case "configNumbers":
+                        // Common slider configs
+                        Slider numbersSlider = (Slider)configsAnchor.lookup("#" + id);
+                        sliderConfig(numbersSlider, (double) config.MAX_NUMBERS);
+                        numbersSlider.setMin(config.MIN_NUMBERS);
+                        numbersSlider.setMax(config.MAX_NUMBERS);
+
+                        // Set default value
+                        Text indicator = ((Text)configsAnchor.lookup("#configNumbersIndicator"));
+                        indicator.setText(String.valueOf(config.getNumbers()));
+                        numbersSlider.setValue(config.getNumbers());
+
+                        // config number slider event listener
+                        numbersSlider.valueProperty().addListener(new ChangeListener<Number>() {
+                            @Override
+                            public void changed(ObservableValue<? extends Number> observableValue, Number initValue, Number newValue) {
+                                int currentNumbers = newValue.intValue();
+                                System.out.println("Slider " + id + " value changed:" + currentNumbers);
+                               indicator.setText(String.valueOf(currentNumbers));
+                            }
+                        });
+
+                        break;
+
+                    case "configSpeed":
+                        // Common slider configs
+                        Slider speedSlider = (Slider)configsAnchor.lookup("#" + id);
+                        sliderConfig(speedSlider, config.MAX_SPEED_INTERVAL);
+                        speedSlider.setMin(config.MIN_SPEED_INTERVAL);
+                        speedSlider.setMax(config.MAX_SPEED_INTERVAL);
+
+                        // Set default value
+                        speedSlider.setValue(config.getSpeedInterval());
+                        Text inidcator = ((Text)configsAnchor.lookup("#configSpeedIndicator"));
+                        inidcator.setText(String.valueOf(Math.round(config.getSpeedInterval() * 1000.0)/1000.0));
+
+                        // config number slider event listener
+                        speedSlider.valueProperty().addListener(new ChangeListener<Number>() {
+                            @Override
+                            public void changed(ObservableValue<? extends Number> observableValue, Number initValue, Number newValue) {
+                                System.out.println("Slider " + id + " value changed:" + newValue);
+                                inidcator.setText(String.valueOf(Math.round(newValue.doubleValue() * 1000.0)/1000.0));
+                            }
+                        });
+
+                        break;
+                }
+            } catch ( NullPointerException e){
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+    }
+
+    public void sliderConfig(Slider s, Double max){
+        s.setShowTickLabels(true);
+        s.setShowTickMarks(true);
+        s.setMajorTickUnit(max/5f);
+        s.setBlockIncrement(max/10f);
     }
 
     public SplitMenuButton setUpLoggedInUser(){
@@ -50,9 +134,11 @@ public class ConfigurationController implements Initializable  {
         return m;
 
     }
+
     public void initData() {
 
         Main.showImage("assets/img/avatar.png", avatarImageView);
+
         SplitMenuButton m = setUpLoggedInUser();
 
         // Create Menu Items programmatically
@@ -102,6 +188,16 @@ public class ConfigurationController implements Initializable  {
             Main.loadScene(e, "profile", true);
         });
 
+
+
+
+
+
+    }
+
+
+    public void runAction(ActionEvent evt){
+        Main.loadScene(evt, "main", false);
     }
 
 
