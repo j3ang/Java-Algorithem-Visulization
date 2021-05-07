bd5df21be5219f04e1046d3a6f4e75bfa06534a6
alex <j3ang.info@gmail.com>
2021-05-07 18:03:42 -0500

    working jar for both mac and windows

--- src/module-info.java
+++ src/module-info.java
@@ -1,14 +1,17 @@
 module AlgorithmViz{
-    requires  javafx.fxml;
-    requires javafx.controls;
-    requires javafx.graphics;
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.media;
+	requires javafx.base;
+	requires javafx.web;
+	requires javafx.swing;
+	requires javafx.fxml;
     requires java.sql;
     requires com.google.gson;
     requires org.apache.commons.codec;
     requires dotenv.java;
     requires java.desktop;
     requires com.google.common;
-
 
 	opens application to javafx.graphics;
 	opens models;
